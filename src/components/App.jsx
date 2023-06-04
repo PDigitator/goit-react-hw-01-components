1390ff2f735044d9928a0bb9b8a601c297a30940
Oleksii Nikolaiev <o.nikolaiev@yahoo.com>
2023-06-04 18:07:47 +0300

    Add Statistic component and styles

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,5 +1,7 @@
-import { Profile } from './Profile/Profile';
+import { Profile } from 'components/Profile/Profile';
+import { Statistics } from 'components/Statistics/Statistics';
 import user from 'data/user.json';
+import data from 'data/data.json';
 
 export const App = () => {
   return (
@@ -11,8 +13,8 @@ export const App = () => {
         avatar={user.avatar}
         stats={user.stats}
       />
-      {/* <Statistics title="Upload stats" stats={stats} />
-      <FriendList friends={friends} />
+      <Statistics title="Upload stats" stats={data} />
+      {/* <FriendList friends={friends} />
       <TransactionHistory items={transactions} />; */}
     </>
   );
--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -20,15 +20,15 @@ export const Profile = ({
             <p className={css.location}>{location}</p>
           </div>
           <ul className={css.stats}>
-            <li className={css.stats__list}>
+            <li className={css.stats__item}>
               <span className={css.label}>Followers</span>
               <span className={css.quantity}>{followers}</span>
             </li>
-            <li className={css.stats__list}>
+            <li className={css.stats__item}>
               <span className={css.label}>Views</span>
               <span className={css.quantity}>{views}</span>
             </li>
-            <li className={css.stats__list}>
+            <li className={css.stats__item}>
               <span className={css.label}>Likes</span>
               <span className={css.quantity}>{likes}</span>
             </li>
