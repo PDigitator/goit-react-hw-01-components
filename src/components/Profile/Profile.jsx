dee1447339f2387d149b69bf9bf096da4788d223
Oleksii Nikolaiev <o.nikolaiev@yahoo.com>
2023-06-04 03:26:45 +0300

    Add Profile styles

--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -19,15 +19,15 @@ export const Profile = ({
       </div>
 
       <ul className={css.stats}>
-        <li>
+        <li className={css.statsList}>
           <span className={css.label}>Followers</span>
           <span className={css.quantity}>{followers}</span>
         </li>
-        <li>
+        <li className={css.statsList}>
           <span className={css.label}>Views</span>
           <span className={css.quantity}>{views}</span>
         </li>
-        <li>
+        <li className={css.statsList}>
           <span className={css.label}>Likes</span>
           <span className={css.quantity}>{likes}</span>
         </li>
